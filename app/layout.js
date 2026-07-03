29aa10e1189dcd0f397212c1c876602fba1c0944
jonathanrelliott <elliott.jonathan.rr@gmail.com>
2026-07-03 10:07:45 -0500

    safri support

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,12 @@ export const metadata = {
   description: 'SIDELINE HIGH ENERGY CAPTURE FRAME ENGINE',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
